5660640e4ec837686ab211e2e28edf7a496f2b3f
BranimirK <bkedacic@hotmail.com>
2018-08-28 20:37:32 +0200

    easy i hard
    dodani easy i hard buttons i funkcije za njih
    dodan numSquares da mogu pratit koliko squares-a ima

--- colorGameProject.js
+++ colorGameProject.js
@@ -1,12 +1,61 @@
-var colors = generateRandomColors(6);
-
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square")
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
 
-colorDisplay.textContent = pickedColor;
+
+easyBtn.addEventListener("click", function(){
+	easyBtn.classList.add("selected");
+	hardBtn.classList.remove("selected");
+	numSquares = 3;
+	colors = generateRandomColors(numSquares);
+	pickedColor=pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i=0; i< squares.length; i++){
+		if(colors[i]){
+			squares[i].style.backgroundColor = colors[i];
+		}
+		else{
+			squares[i].style.display = "none";
+		}
+	}
+});
+
+hardBtn.addEventListener("click", function(){
+	hardBtn.classList.add("selected");
+	easyBtn.classList.remove("selected");
+	numSquares = 6;
+	colors =generateRandomColors(numSquares);
+	pickedColor=pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i=0; i< squares.length; i++){
+			squares[i].style.backgroundColor = colors[i];
+			squares[i].style.display = "block";
+	}
+});
+
+
+resetButton.addEventListener("click", function(){
+	//generirati nove boje
+	colors = generateRandomColors(numSquares);
+	//odabrati novu boju iz arraya
+	pickedColor = pickColor();
+	//promjeniti colorDisplay da bude kao pickedColor
+	colorDisplay.textContent = pickedColor;
+	//promjeniti boje squares-a,
+	for(var i=0; i<squares.length; i++){
+		squares[i].style.backgroundColor = colors[i];
+	}
+	h1.style.backgroundColor = "#232323";
+});
+
+colorDisplay.textContent = pickedColor; // postavlja The blablabla-boja odma cim ucita
 
 for (var i=0; i<squares.length; i++){
 	//dodjela boja squares
@@ -18,8 +67,9 @@ for (var i=0; i<squares.length; i++){
 	//console.log(clickedColor, pickedColor);
 	if (clickedColor === pickedColor){
 		messageDisplay.textContent = "Correct";
+		resetButton.textContent = "Play Again?"
 		changeColors(clickedColor);
-		h1.style.backgroundColor = pickedColor;
+		h1.style.backgroundColor = clickedColor;
 	}
 	else{
 		this.style.backgroundColor = "#232323";
@@ -37,7 +87,7 @@ function changeColors(color){
 
 function pickColor(){
 	var random = Math.floor(Math.random() * colors.length);
-	return colors[random];
+	return colors[random];  // vraca random broj od colors, i taj ce postat picked
 }
 
 function generateRandomColors(num){
@@ -59,6 +109,5 @@ function randomColor(){
 	var g = Math.floor(Math.random() * 256);
 	//odabrati plavu od 0-255
 	var b = Math.floor(Math.random() * 256);
-
 	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
