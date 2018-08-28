6d370f5848ca2cd951b113d1d495d59807fcb0ea
BranimirK <bkedacic@hotmail.com>
2018-08-28 19:38:00 +0200

    pickColor, randomColor, GenerateRandomColors
    dodana funkcija pickColor za odabir jedne random boje u arrayu
    dodana funkcija randomColor za kreiranje rgb ( random, random, random)
    dodana funkcija generateRandomColors koja generira neki broj random rgb boja i sprema ih u array, zatim sprema taj array u colors

--- colorGameProject.js
+++ colorGameProject.js
@@ -1,16 +1,10 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(234, 25, 211)",
-	"rgb(32, 235, 231)",
-	"rgb(200, 100, 10)",
-	"rgb(43, 65, 189)"
-	]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square")
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -21,9 +15,11 @@ for (var i=0; i<squares.length; i++){
 	squares[i].addEventListener("click", function(){
 	//dohvatit boju squarea i usporediti ju s odabranom
 	var clickedColor = this.style.backgroundColor;
+	//console.log(clickedColor, pickedColor);
 	if (clickedColor === pickedColor){
 		messageDisplay.textContent = "Correct";
 		changeColors(clickedColor);
+		h1.style.backgroundColor = pickedColor;
 	}
 	else{
 		this.style.backgroundColor = "#232323";
@@ -39,5 +35,30 @@ function changeColors(color){
 	}
 }
 
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
 
+function generateRandomColors(num){
+	//naparviti array
+	var arr = [];
+	//dodati random colors u array
+	for (i=0; i<num; i++){
+		//get color i pushati ih u array
+		arr.push(randomColor());
+	}
+	//return array
+	return arr;
+}
 
+function randomColor(){
+	//odabrati crvenu od 0-255
+	var r = Math.floor(Math.random() * 256);
+	//odabrati zelenu od 0-255
+	var g = Math.floor(Math.random() * 256);
+	//odabrati plavu od 0-255
+	var b = Math.floor(Math.random() * 256);
+
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}
